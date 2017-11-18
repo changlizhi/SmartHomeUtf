26d323d4522d5dc713243e85409e1c8cf66eb2a3
changlz <changlizhi@aliyun.com>
2017-11-18 21:30:11 +0800

    add sleep

--- monitor/monitor_task.c
+++ monitor/monitor_task.c
@@ -389,10 +389,11 @@ static void *PlayTask_Pressdown(void *arg)
 						{
 							
 							PlayVoice("startplay.wav",0);
-							PlayVoice("ceshikaishibofang.wav",0);
 
-							Sleep(300);
-							
+							Sleep(600);
+							PlayVoice("ceshikaishibofang.wav",0);
+							Sleep(600);
+
 							playstate = PlayVoice("shock.mp3",1);
 							if(playstate == 0)
 							{
