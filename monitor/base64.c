ca91ea6650311aefd23056615115bb2609bca6f4
changlz <changlizhi@aliyun.com>
2017-12-17 14:42:18 +0800

    base64测试5

--- monitor/base64.c
+++ monitor/base64.c
@@ -1,59 +1,134 @@
-static const char *ALPHA_BASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
- 
-char *encode(const char *buf, const long size, char *base64Char) {
-    int a = 0;
-    int i = 0;
-    while (i < size) {
-        char b0 = buf[i++];
-        char b1 = (i < size) ? buf[i++] : 0;
-        char b2 = (i < size) ? buf[i++] : 0;
-         
-        int int63 = 0x3F; //  00111111
-        int int255 = 0xFF; // 11111111
-        base64Char[a++] = ALPHA_BASE[(b0 >> 2) & int63];
-        base64Char[a++] = ALPHA_BASE[((b0 << 4) | ((b1 & int255) >> 4)) & int63];
-        base64Char[a++] = ALPHA_BASE[((b1 << 2) | ((b2 & int255) >> 6)) & int63];
-        base64Char[a++] = ALPHA_BASE[b2 & int63];
+#include <stdio.h>
+#include <string.h>
+
+// 全局常量定义
+const char * base64char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const char padding_char = '=';
+
+/*编码代码
+* const unsigned char * sourcedata， 源数组
+* char * base64 ，码字保存
+*/
+
+/** 在字符串中查询特定字符位置索引
+* const char *str ，字符串
+* char c，要查找的字符
+*/
+inline int num_strchr(const char *str, char c) //
+{
+    const char *pindex = strchr(str, c);
+    if (NULL == pindex){
+        return -1;
     }
-    switch (size % 3) {
-        case 1:
-            base64Char[--a] = '=';
-        case 2:
-            base64Char[--a] = '=';
-    }
-    return base64Char;
+    return pindex - str;
 }
- 
-char *decode(const char *base64Char, const long base64CharSize, char *originChar, long originCharSize) {
-    int toInt[128] = {-1};
-    for (int i = 0; i < 64; i++) {
-        toInt[ALPHA_BASE[i]] = i;
-    }
-    int int255 = 0xFF;
-    int index = 0;
-    for (int i = 0; i < base64CharSize; i += 4) {
-        int c0 = toInt[base64Char[i]];
-        int c1 = toInt[base64Char[i + 1]];
-        originChar[index++] = (((c0 << 2) | (c1 >> 4)) & int255);
-        if (index >= originCharSize) {
-            return originChar;
+/* 解码
+* const char * base64 码字
+* unsigned char * dedata， 解码恢复的数据
+*/
+int base64_decode(const char * base64, unsigned char * dedata)
+{
+    int i = 0, j=0;
+    int trans[4] = {0,0,0,0};
+    for (;base64[i]!='\0';i+=4){
+        // 每四个一组，译码成三个字符
+        trans[0] = num_strchr(base64char, base64[i]);
+        trans[1] = num_strchr(base64char, base64[i+1]);
+        // 1/3
+        dedata[j++] = ((trans[0] << 2) & 0xfc) | ((trans[1]>>4) & 0x03);
+
+        if (base64[i+2] == '='){
+            continue;
         }
-        int c2 = toInt[base64Char[i + 2]];
-        originChar[index++] = (((c1 << 4) | (c2 >> 2)) & int255);
-        if (index >= originCharSize) {
-            return originChar;
+        else{
+            trans[2] = num_strchr(base64char, base64[i + 2]);
         }
-        int c3 = toInt[base64Char[i + 3]];
-        originChar[index++] = (((c2 << 6) | c3) & int255);
+        // 2/3
+        dedata[j++] = ((trans[1] << 4) & 0xf0) | ((trans[2] >> 2) & 0x0f);
+
+        if (base64[i + 3] == '='){
+            continue;
+        }
+        else{
+            trans[3] = num_strchr(base64char, base64[i + 3]);
+        }
+
+        // 3/3
+        dedata[j++] = ((trans[2] << 6) & 0xc0) | (trans[3] & 0x3f);
     }
-    return originChar;
+
+    dedata[j] = '\0';
+
+    return 0;
 }
 
-int main(int argc,char *argv[]){
-    char *base64Char = "aHR0cDovL3Bpbmx2LmFtY2hpcy5jb20vbWhzeV93ZWIvbGVpeGluZy95aW5waW4xLm1wMy50YXIuZ3o=";
-    long base64CharSize = 80;
-    char *originChar = "";
-    long originCharSize = 1024;
-    decode(base64Char,base64CharSize,originChar,originCharSize)
-    return 0
+int base64_encode(const unsigned char * sourcedata, char * base64)
+{
+    int i=0, j=0;
+    unsigned char trans_index=0;    // 索引是8位，但是高两位都为0
+    const int datalength = strlen((const char*)sourcedata);
+    for (; i < datalength; i += 3){
+        // 每三个一组，进行编码
+        // 要编码的数字的第一个
+        trans_index = ((sourcedata[i] >> 2) & 0x3f);
+        base64[j++] = base64char[(int)trans_index];
+        // 第二个
+        trans_index = ((sourcedata[i] << 4) & 0x30);
+        if (i + 1 < datalength){
+            trans_index |= ((sourcedata[i + 1] >> 4) & 0x0f);
+            base64[j++] = base64char[(int)trans_index];
+        }else{
+            base64[j++] = base64char[(int)trans_index];
+
+            base64[j++] = padding_char;
+
+            base64[j++] = padding_char;
+
+            break;   // 超出总长度，可以直接break
+        }
+        // 第三个
+        trans_index = ((sourcedata[i + 1] << 2) & 0x3c);
+        if (i + 2 < datalength){ // 有的话需要编码2个
+            trans_index |= ((sourcedata[i + 2] >> 6) & 0x03);
+            base64[j++] = base64char[(int)trans_index];
+
+            trans_index = sourcedata[i + 2] & 0x3f;
+            base64[j++] = base64char[(int)trans_index];
+        }
+        else{
+            base64[j++] = base64char[(int)trans_index];
+
+            base64[j++] = padding_char;
+
+            break;
+        }
+    }
+
+    base64[j] = '\0';
+
+    return 0;
+}
+
+// 测试
+int main()
+{
+    const unsigned char str[] = "http://pinlv.amchis.com/mhsy_web/leixing/yinpin1.mp3.tar.gz";
+    const unsigned char dest[] = "aHR0cDovL3Bpbmx2LmFtY2hpcy5jb20vbWhzeV93ZWIvbGVpeGluZy95aW5waW4xLm1wMy50YXIuZ3o=";
+    const unsigned char *sourcedata = str ;
+    const unsigned char *destdata = dest ;
+    char base64[128];
+    base64_encode(sourcedata, base64);
+
+    printf("编码：%s\n",base64);
+    printf("预留：%s\n",destdata);
+
+    char dedata[128];
+
+    base64_decode(base64, (unsigned char*)dedata);
+
+    printf("译码：%s", dedata);
+
+    getchar();
+    getchar();
+    return 0;
 }
