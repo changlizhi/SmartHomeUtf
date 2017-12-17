3c92f14589f5c1453937f8de0ec175daf29ab9fd
changlizhi <changlizhi@aliyun.com>
2017-12-17 07:49:46 -0500

    qingqiu http

--- monitor/client.c
+++ monitor/client.c
@@ -22,15 +22,17 @@ int cilent_main(char buf[BUFSIZE]);
 int main(int argc,char *argv[])
 {
     char retbuf[BUFSIZE];
-    int retsize = cilent_main(retbuf);
-    printf("**retsize***%d\n",retsize);
-    printf("retbuf-------%s\n",retbuf);
+    cilent_main(retbuf);
 
     char delims[] = "\n";
     char *result = NULL;
     result = strtok(retbuf, delims );
     while( result != NULL ) {
-        printf( "result is \"%s\"\n", result );
+        printf( "r--------------%s", result );
+        char *httpstr = strstr(result,(unsigned char *)"HTTP");
+        if(httpstr != NULL){
+          printf("httpstr-----%s",httpstr);
+        }
         result = strtok( NULL, delims );
     }
 
@@ -45,7 +47,7 @@ int cilent_main(char buf[])
     socklen_t len;
     fd_set   t_set1;
     char *toke;
-    char *ps[1024];
+    char *ps[BUFSIZE];
     int t = 0;
     int j = 0;
     struct timeval  tv;
@@ -64,36 +66,38 @@ int cilent_main(char buf[])
         printf("连接到服务器失败,connect error!\n");
         exit(0);
     }
-    printf("与远端建立了连接\n");
-    memset(str2, 0, 4096);
+    //printf("与远端建立了连接\n");
+    memset(str2, 0, BUFSIZE);
     strcat(str2, "theDataToPost");
     str=(char *)malloc(128);
     len = strlen(str2);
-    sprintf(str, "%d", len);
-    memset(str1, 0, 1024);
+   // sprintf(str, "%d", len);
+    memset(str1, 0, BUFSIZE);
     strcat(str1, "GET /mhsy_web/huoqulianjie?sn=1234 HTTP/1.1\n");
-    strcat(str1, "Host:192.168.0.102 \n");
+    strcat(str1, "Host:");
+    strcat(str1, IPSTR);
+    strcat(str1, " \n");
  //       strcat(str1, "Host:IPSTR\n");
     strcat(str1, "Content-Type: text/plain\n");
     strcat(str1, "Content-Length: ");
     strcat(str1, str);
     strcat(str1, "\n\n");
-    printf("%s\n",str);
+//    printf("%s\n",str);
  
     strcat(str1, str2);
     strcat(str1, "\r\n\r\n");
-    printf("%s\n",str1);
+//    printf("%s\n",str1);
  
-    printf("****************\n");
+//    printf("****************\n");
     ret = write(sockfd,str1,strlen(str1));
     if (ret < 0) {
         printf("发送失败！错误代码是%d，错误信息是'%s'\n",errno, strerror(errno));
         exit(0);
     }else{
-        printf("消息发送成功，共发送了%d个字节！\n\n", ret);
+//        printf("消息发送成功，共发送了%d个字节！\n\n", ret);
     }
-    printf("****************\n");
-    printf("*****************%s\n",str1);
+//    printf("****************\n");
+//    printf("*****************%s\n",str1);
     FD_ZERO(&t_set1);
     FD_SET(sockfd, &t_set1);
  
@@ -101,17 +105,17 @@ int cilent_main(char buf[])
     tv.tv_sec= 0;
     tv.tv_usec= 0;
     h= 0;
-    printf("--------------->1\n");
+//    printf("--------------->1\n");
     h= select(sockfd +1, &t_set1, NULL, NULL, &tv);
-    printf("--------------->2\n");
+//    printf("--------------->2\n");
     if (h < 0) {
         close(sockfd);
         printf("在读取数据报文时SELECT检测到异常，该异常导致线程终止！\n");
         return -1;
     }
     if (h > 0){
-        memset(buf, 0, 1024);
-        i= read(sockfd, buf,1024 );
+        memset(buf, 0, BUFSIZE);
+        i= read(sockfd, buf,BUFSIZE );
         if (i<0){
             close(sockfd);
             printf("读取数据报文时发现远端关闭，该线程终止！\n");
