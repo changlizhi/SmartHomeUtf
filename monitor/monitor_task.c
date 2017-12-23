54460a3cdbe545b77f5d0b727eaf403dc2021f45
changlz <changlizhi@aliyun.com>
2017-12-23 19:08:03 +0800

    增加base64部分

--- monitor/monitor_task.c
+++ monitor/monitor_task.c
@@ -43,6 +43,69 @@
 #include "../uplink/svrcomm.h"
 
 
+// 全局常量定义
+const char * base64char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const char padding_char = '=';
+
+/*编码代码
+* const unsigned char * sourcedata， 源数组
+* char * base64 ，码字保存
+*/
+
+/** 在字符串中查询特定字符位置索引
+* const char *str ，字符串
+* char c，要查找的字符
+*/
+inline int num_strchr(const char *str, char c) //
+{
+    const char *pindex = strchr(str, c);
+    if (NULL == pindex){
+        return -1;
+    }
+    return pindex - str;
+}
+/* 解码
+* const char * base64 码字
+* unsigned char * dedata， 解码恢复的数据
+*/
+int base64_decode(const char * base64, unsigned char * dedata)
+{
+    int i = 0, j=0;
+    int trans[4] = {0,0,0,0};
+    for (;base64[i]!='\0';i+=4){
+        // 每四个一组，译码成三个字符
+        trans[0] = num_strchr(base64char, base64[i]);
+        trans[1] = num_strchr(base64char, base64[i+1]);
+        // 1/3
+        dedata[j++] = ((trans[0] << 2) & 0xfc) | ((trans[1]>>4) & 0x03);
+
+        if (base64[i+2] == '='){
+            continue;
+        }
+        else{
+            trans[2] = num_strchr(base64char, base64[i + 2]);
+        }
+        // 2/3
+        dedata[j++] = ((trans[1] << 4) & 0xf0) | ((trans[2] >> 2) & 0x0f);
+
+        if (base64[i + 3] == '='){
+            continue;
+        }
+        else{
+            trans[3] = num_strchr(base64char, base64[i + 3]);
+        }
+
+        // 3/3
+        dedata[j++] = ((trans[2] << 6) & 0xc0) | (trans[3] & 0x3f);
+    }
+
+    dedata[j] = '\0';
+
+    return 0;
+}
+
+
+
 #define IPSTR "10.0.0.104"
 #define PORT 8989
 #define BUFSIZE 1024
@@ -413,6 +476,10 @@ static void *DownLoadMusicTask_Monitor(void *arg)
                 currentButtonState = 0;
 
                 memset(downloadcmd,0,512);
+
+                //base64解密之后得到url
+
+
                 sprintf(downloadcmd,"sh /opt/work/musicdownload.sh %s",audiourl);
                 system(downloadcmd);
                 PlayVoice("enddownload.wav",0);
